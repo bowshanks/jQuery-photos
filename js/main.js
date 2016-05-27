2b2af24317a9e26ba086db12ad33afd9cfa01eef
Sam Biggs <harrbiggs@gmail.com>
2016-05-26 19:27:53 -0600

    added a clear button and clear function

--- js/main.js
+++ js/main.js
@@ -16,27 +16,31 @@ $(document).ready(function() {
         search = $('#searchbar');
 
 
-        photoApp.append(photoContainer);
+    photoApp.append(photoContainer);
 
 
 
-    function getPhotos(){
+    function getPhotos() {
 
-     var key = '77h7td8ewaukmcjtcwp8awwj', // Api Key
-         searchTerm = search.val(), // Search term from input field
-         photoUrl = 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=' + searchTerm;
+        var key = '77h7td8ewaukmcjtcwp8awwj', // Api Key
+            searchTerm = search.val(), // Search term from input field
+            photoUrl = 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=' + searchTerm;
 
         // This is a example of using the jQuery ajax method to get data
-         $.ajax({
-             url: photoUrl,
-             type: "GET",
-             dataType: "json",
-             headers: {"Api-Key": key}
+        $.ajax({
+            url: photoUrl,
+            type: "GET",
+            dataType: "json",
+            headers: {
+                "Api-Key": key
+            }
 
-         }).done(makePhotoSection).fail( function (msg) { alert(msg); })
- }
+        }).done(makePhotoSection).fail(function(msg) {
+            alert(msg);
+        })
+    }
 
-    function makePhotoSection(data){
+    function makePhotoSection(data) {
         var responseData = data.images;
 
         // Clear input field
@@ -44,17 +48,17 @@ $(document).ready(function() {
 
 
 
-            // Here we are using map to iterate over the array
+        // Here we are using map to iterate over the array
 
-          responseData.map(function(item,index){
-              var url = item.display_sizes[0].uri,
-                      img = $('<img class="image">').hide();
-                      img.attr('src', url);
-                      photoApp.append(img);
-                      img.show('normal');
+        responseData.map(function(item, index) {
+            var url = item.display_sizes[0].uri,
+                img = $('<img class="image">').hide();
+            img.attr('src', url);
+            photoApp.append(img);
+            img.show('normal');
 
 
-          });
+        });
 
         // We could use a forEach
 
@@ -70,25 +74,31 @@ $(document).ready(function() {
         // We could use a for loop
 
         //for (var i = 0; i < responseData.length; i++) {
-            //    var url = responseData[i].display_sizes[0].uri,
-            //        img = $('<img class="image">').hide();
-            //        img.attr('src', url);
-            //        photoApp.append(img);
-            //        img.show('normal');
-            //}
+        //    var url = responseData[i].display_sizes[0].uri,
+        //        img = $('<img class="image">').hide();
+        //        img.attr('src', url);
+        //        photoApp.append(img);
+        //        img.show('normal');
+        //}
     }
 
     // Here we are watching for the return key to be pressed
 
-        search.on('keyup', function(event) {
-            if (event.which == 13) {
-                getPhotos();
-            }
-        });
+    search.on('keyup', function(event) {
+        if (event.which == 13) {
+            getPhotos();
+        }
+    });
 
-        // Here we are watching for a click
+    // Here we are watching for a click
 
-        imageClick.on('click', getPhotos);
+    imageClick.on('click', getPhotos);
+
+    // This function clears the page (removes the called images) when the clear button is pressed
+    $('#clear-button').click (function(){
+      $('img').remove('.image');
+    });
+
 
 
 });
